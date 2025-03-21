5578847f8d81b56f7b27d6829b90c62ee0869c8f
Zainds <beat1097@mail.ru>
2025-03-21 15:02:23 +0700

    Complete reverse string task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("")
+    // split превращает строку в массив букв
+    // reverse переворачивает массив
+    // join собирает буквы обратно в строку
 };
 
 // Do not edit below this line
