c82dc54138004649b8f78839b50f2b518f9d316a
Zainds <beat1097@mail.ru>
2025-03-27 21:22:00 +0700

    Complete Palindrome task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const fotmattedString = str
+        .toLowerCase()
+        .split('')
+        .filter((item) => item != '!' && item != ',' && item != '.' && item != ' ' )
+        .join()
+    const reversedFormattedString = fotmattedString
+        .split('')
+        .reverse()
+        .join('')
+    return fotmattedString == reversedFormattedString
 };
 
 // Do not edit below this line
