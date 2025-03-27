f3980bd9f1b68b4a53425d4602c6f3366b7e6cb9
Zainds <beat1097@mail.ru>
2025-03-27 22:03:25 +0700

    Complete findTheOldest Task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objArr) {
+    return objArr.reduce((oldest, item) => {
+        let itemDeath = 2025
+        let oldestDeath = 2025
+        if(item.yearOfDeath) itemDeath = item.yearOfDeath
+        if(oldest.yearOfDeath) oldestDeath = oldest.yearOfDeath
+        if(
+            itemDeath - item.yearOfBirth > oldestDeath - oldest.yearOfBirth
+        ) return item
+        
+        return oldest
+    }, {
+        name: "lolka",
+        yearOfBirth: 1,
+        yearOfDeath: 1,})
 };
 
 // Do not edit below this line
