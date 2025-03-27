7948706486b243d1747141c4618d2a60fe29fc5d
Zainds <beat1097@mail.ru>
2025-03-27 21:36:20 +0700

    Complete fibonacci task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS"
+    let previous = 0
+    let current = 1
+    for(let i = 1; i <=n; i ++){     
+        let temp = current
+        current += previous
+        previous = temp
+    }
+    return previous
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
